29f23b297546488411c3083319ae3e6b0e81b7ed
Artem <artemka00913x@gmail.com>
2025-11-06 13:14:11 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.androidx.navigation.safeargs.kotlin)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.an_app_for_runners_and_cyclists"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.an_app_for_runners_and_cyclists"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "APP_VERSION", "\"1.0.0\"")
+        buildConfigField("boolean", "LOG_ENABLED", "true")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+
+    // Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Activity & Fragment
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // В dependencies блока build.gradle.kts добавляем:
+    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation ("com.google.android.material:material:1.6.0")
+    // Location Services
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/an_app_for_runners_and_cyclists/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.an_app_for_runners_and_cyclists
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.an_app_for_runners_and_cyclists.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+/* "Зажги, гаси, насри и смой,
+И в этом вся наука:
+Чтоб буквы видеть, милый мой,
+Сумей говно сменить мочой. Иди, работай, сука!"
+*/
+
+}
